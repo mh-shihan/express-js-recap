b765af034d9531216d6188a207d0f6108ca582e8
MHShihan <mahmodul.no.1@gmail.com>
2025-03-07 00:08:47 +0600

    fetch data using asycn await

--- express-js-client/src/App.jsx
+++ express-js-client/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import InputForm from "./components/input-form/InputForm";
 
 function App() {
@@ -11,10 +12,28 @@ function App() {
     console.log("Input form submitted", userInfo);
   };
 
+  useEffect(() => {
+    fetchPeople();
+  }, []);
+
+  const fetchPeople = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/");
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
   return (
     <>
       <h1 className="text-5xl font-bold text-center">Express Recap</h1>
       <InputForm handleInputFormSubmit={handleInputFormSubmit}></InputForm>
+      <div></div>
     </>
   );
 }
