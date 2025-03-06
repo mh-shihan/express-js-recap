905cb55eec2f3a3f2ae659fd01c6ab5f24cf555f
MHShihan <mahmodul.no.1@gmail.com>
2025-03-06 23:38:37 +0600

    Create handleInputFormSubmit function

--- express-js-client/src/App.jsx
+++ express-js-client/src/App.jsx
@@ -1,10 +1,20 @@
 import InputForm from "./components/input-form/InputForm";
 
 function App() {
+  const handleInputFormSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+
+    const userInfo = { name, email };
+    console.log("Input form submitted", userInfo);
+  };
+
   return (
     <>
       <h1 className="text-5xl font-bold text-center">Express Recap</h1>
-      <InputForm></InputForm>
+      <InputForm handleInputFormSubmit={handleInputFormSubmit}></InputForm>
     </>
   );
 }
--- express-js-client/src/components/input-form/InputForm.jsx
+++ express-js-client/src/components/input-form/InputForm.jsx
@@ -1,7 +1,10 @@
-const InputForm = () => {
+const InputForm = ({ handleInputFormSubmit }) => {
   return (
     <div className="flex justify-center mt-24">
-      <form className="flex flex-col gap-4 w-1/4 border-2 border-fuchsia-700 p-10 rounded-lg bg-slate-400">
+      <form
+        onSubmit={handleInputFormSubmit}
+        className="flex flex-col gap-4 w-1/4 border-2 border-fuchsia-700 p-10 rounded-lg bg-slate-400"
+      >
         <input className="rounded-md" type="text" name="name" id="" />
         <input className=" rounded-md" type="email" name="email" id="" />
         <input
