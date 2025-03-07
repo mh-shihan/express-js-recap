b06ac45935be0a613f456e17356da42571bcaa21
MHShihan <mahmodul.no.1@gmail.com>
2025-03-07 06:45:46 +0600

    people card created

--- express-js-client/src/App.jsx
+++ express-js-client/src/App.jsx
@@ -1,14 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import InputForm from "./components/input-form/InputForm";
+import People from "./components/people/People";
 
 function App() {
+  const [peoples, setPeoples] = useState([]);
   const handleInputFormSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
     const name = form.name.value;
     const email = form.email.value;
+    const age = form.age.value;
 
-    const userInfo = { name, email };
+    const userInfo = { name, email, age };
     console.log("Input form submitted", userInfo);
   };
 
@@ -18,11 +21,12 @@ function App() {
 
   const fetchPeople = async () => {
     try {
-      const response = await fetch("http://localhost:5000/");
+      const response = await fetch("http://localhost:5000/peoples");
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
       const data = await response.json();
+      setPeoples(data);
       console.log(data);
     } catch (error) {
       console.error("Error fetching data: ", error);
@@ -33,6 +37,14 @@ function App() {
     <>
       <h1 className="text-5xl font-bold text-center">Express Recap</h1>
       <InputForm handleInputFormSubmit={handleInputFormSubmit}></InputForm>
+      <p className="mt-20 mb-6 text-3xl font-bold text-center ">
+        Total People : {peoples.length}
+      </p>
+      <div className="grid grid-cols-5 gap-2 mx-20  mb-20">
+        {peoples.map((people) => (
+          <People key={people.id} people={people}></People>
+        ))}
+      </div>
       <div></div>
     </>
   );
--- express-js-client/src/components/input-form/InputForm.jsx
+++ express-js-client/src/components/input-form/InputForm.jsx
@@ -5,8 +5,27 @@ const InputForm = ({ handleInputFormSubmit }) => {
         onSubmit={handleInputFormSubmit}
         className="flex flex-col gap-4 w-1/4 border-2 border-fuchsia-700 p-10 rounded-lg bg-slate-400"
       >
-        <input className="rounded-md" type="text" name="name" id="" />
-        <input className=" rounded-md" type="email" name="email" id="" />
+        <input
+          className="rounded-md"
+          type="text"
+          name="name"
+          placeholder="name"
+          id=""
+        />
+        <input
+          className=" rounded-md"
+          type="email"
+          name="email"
+          placeholder="email"
+          id=""
+        />
+        <input
+          className=" rounded-md"
+          type="number"
+          name="age"
+          placeholder="age"
+          id=""
+        />
         <input
           className="border border-black rounded-md font-medium cursor-pointer bg-emerald-400"
           type="submit"
