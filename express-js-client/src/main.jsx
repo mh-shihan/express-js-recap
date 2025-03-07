7f7fb437709b2a5fb88d6ed39dc9a5ac1ed5a251
MHShihan <mahmodul.no.1@gmail.com>
2025-03-07 07:02:36 +0600

    Create main layout

--- express-js-client/src/main.jsx
+++ express-js-client/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import InputForm from "./components/input-form/InputForm.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      { path: "input-form", element: <InputForm></InputForm> },
+      { path: "input-form", element: <InputForm></InputForm> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
